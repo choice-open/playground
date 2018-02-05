ce390cd2b6c08f62b2b5e6669ca4597ee7ab2799
Yiming Chen <dsdshcym@gmail.com>
2018-02-05 20:25:19 +0800

    feature: add Surveys.get_survey!/1 function to get the stub survey

--- simple_cform/lib/simple_cform/surveys/surveys.ex
+++ simple_cform/lib/simple_cform/surveys/surveys.ex
@@ -9,6 +9,49 @@ defmodule SimpleCform.Surveys do
   alias SimpleCform.Surveys.SelectAnswer
 
   @doc """
+  It's an in-memory repo,
+  which always returns the same result.
+  """
+  def get_survey!(_id) do
+    %{
+      id: 1,
+      title: "程序员信仰测试",
+      questions: [
+        %{
+          id: 1,
+          type: "select",
+          title: "请选择你最喜欢的语言",
+          required: true,
+          options: [
+            %{
+              id: 1,
+              content: "PHP"
+            },
+            %{
+              id: 2,
+              content: "Ruby"
+            },
+            %{
+              id: 3,
+              content: "Elixir"
+            },
+            %{
+              id: 4,
+              content: "JavaScript"
+            }
+          ]
+        },
+        %{
+          id: 2,
+          type: "fill",
+          title: "请填写你喜欢它的原因",
+          required: false
+        }
+      ]
+    }
+  end
+
+  @doc """
   Creates a response for a existing survey.
   """
   def create_response(survey, answers_attrs) do
--- simple_cform/test/simple_cform/surveys/surveys_test.exs
+++ simple_cform/test/simple_cform/surveys/surveys_test.exs
@@ -3,6 +3,49 @@ defmodule SimpleCform.SurveysTest do
 
   alias SimpleCform.Surveys
 
+  describe "get_survey!/1" do
+    test "always returns expected results" do
+      expected_survey = %{
+        id: 1,
+        title: "程序员信仰测试",
+        questions: [
+          %{
+            id: 1,
+            type: "select",
+            title: "请选择你最喜欢的语言",
+            required: true,
+            options: [
+              %{
+                id: 1,
+                content: "PHP"
+              },
+              %{
+                id: 2,
+                content: "Ruby"
+              },
+              %{
+                id: 3,
+                content: "Elixir"
+              },
+              %{
+                id: 4,
+                content: "JavaScript"
+              }
+            ]
+          },
+          %{
+            id: 2,
+            type: "fill",
+            title: "请填写你喜欢它的原因",
+            required: false
+          }
+        ]
+      }
+
+      assert Surveys.get_survey!(1) == expected_survey
+    end
+  end
+
   describe "create_response/2" do
     alias SimpleCform.Surveys.SelectAnswer
     alias SimpleCform.Surveys.FillAnswer
