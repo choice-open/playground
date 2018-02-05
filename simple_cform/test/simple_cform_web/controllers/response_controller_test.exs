2bdd859e65a761446a4d2035f89f174062a1a301
Yiming Chen <dsdshcym@gmail.com>
2018-02-05 20:59:40 +0800

    test: add missing answers parameter in ResponseControllerTest

--- simple_cform/test/simple_cform_web/controllers/response_controller_test.exs
+++ simple_cform/test/simple_cform_web/controllers/response_controller_test.exs
@@ -3,7 +3,7 @@ defmodule SimpleCformWeb.ResponseControllerTest do
 
   describe "POST /v1/responses" do
     test "returns 201 :created", %{conn: conn} do
-      conn = post(conn, "/v1/responses", survey_id: 1)
+      conn = post(conn, "/v1/responses", survey_id: 1, answers: [])
       assert conn.status == 201
     end
 
