d3bbec5ade2175bfbe8321928d2e48f35b51cc50
Yiming Chen <dsdshcym@gmail.com>
2018-02-05 21:02:18 +0800

    feature: create and render answers in ResponseController.create

--- simple_cform/lib/simple_cform_web/controllers/response_controller.ex
+++ simple_cform/lib/simple_cform_web/controllers/response_controller.ex
@@ -1,9 +1,15 @@
 defmodule SimpleCformWeb.ResponseController do
   use SimpleCformWeb, :controller
 
-  def create(conn, _params) do
-    conn
-    |> put_status(:created)
-    |> render("create.json")
+  alias SimpleCform.Surveys
+
+  def create(conn, %{"survey_id" => survey_id, "answers" => answers_attrs}) do
+    survey = Surveys.get_survey!(survey_id)
+
+    with {:ok, response} <- Surveys.create_response(survey, answers_attrs) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", response: response)
+    end
   end
 end
--- simple_cform/lib/simple_cform_web/views/response_view.ex
+++ simple_cform/lib/simple_cform_web/views/response_view.ex
@@ -1,12 +1,29 @@
 defmodule SimpleCformWeb.ResponseView do
   use SimpleCformWeb, :view
 
-  def render("create.json", _assigns) do
+  alias SimpleCform.Surveys.SelectAnswer
+  alias SimpleCform.Surveys.FillAnswer
+
+  def render("create.json", %{response: response}) do
     %{
-      answers: [
-        %{question_id: 1, selected_options: [1]},
-        %{question_id: 2, content: "Test Content"}
-      ]
+      answers:
+        for answer <- response.answers do
+          answer |> to_json()
+        end
+    }
+  end
+
+  defp to_json(%SelectAnswer{} = select_answer) do
+    %{
+      question_id: select_answer.question_id,
+      selected_options: select_answer.selected_options
+    }
+  end
+
+  defp to_json(%FillAnswer{} = fill_answer) do
+    %{
+      question_id: fill_answer.question_id,
+      content: "Test Content"
     }
   end
 end
