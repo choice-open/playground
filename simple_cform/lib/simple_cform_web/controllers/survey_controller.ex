f86be002bbde18771d07ad57b28ffa10a2e757dc
Yiming Chen <dsdshcym@gmail.com>
2018-02-05 20:27:21 +0800

    refactor: use Surveys.get_survey! to get the stub in SurveyController

--- simple_cform/lib/simple_cform_web/controllers/survey_controller.ex
+++ simple_cform/lib/simple_cform_web/controllers/survey_controller.ex
@@ -1,7 +1,9 @@
 defmodule SimpleCformWeb.SurveyController do
   use SimpleCformWeb, :controller
+  alias SimpleCform.Surveys
 
-  def show(conn, _params) do
-    render(conn, "show.json")
+  def show(conn, %{"id" => id}) do
+    survey = Surveys.get_survey!(id)
+    render(conn, "show.json", survey: survey)
   end
 end
