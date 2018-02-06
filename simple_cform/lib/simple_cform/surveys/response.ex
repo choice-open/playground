93ac0215007a7d6e8d2e07d70559f81caf00748a
Yiming Chen <dsdshcym@gmail.com>
2018-02-06 20:36:46 +0800

    feature: add to_multi/1 function for Response
    - Response is like a Form Object (in Rails world), receives a form and returns a
      Multi for Repo to do the real action

--- simple_cform/lib/simple_cform/surveys/response.ex
+++ simple_cform/lib/simple_cform/surveys/response.ex
@@ -1,12 +1,73 @@
 defmodule SimpleCform.Surveys.Response do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.Multi
   alias SimpleCform.Surveys
   alias SimpleCform.Surveys.Response
+  alias SimpleCform.Surveys.SelectAnswer
+  alias SimpleCform.Surveys.FillAnswer
 
   embedded_schema do
     field(:survey_id, :integer)
     field(:answers, {:array, :map})
+  end
+
+  def to_multi(changeset) do
+    with {_, survey_id} <- changeset |> fetch_field(:survey_id),
+         survey <- survey_id |> Surveys.get_survey!(),
+         {_, answers_attrs} <- changeset |> fetch_field(:answers) do
+      answers_attrs
+      |> Enum.reduce(Multi.new(), fn attr, multi ->
+        # HACK: support both types of question_id from controller and test
+        question_id = attr["question_id"] || attr[:question_id]
+        question = get_question(question_id, survey)
+        create_answer(multi, question, attr)
+      end)
+    end
+  end
+
+  @doc false
+  defp create_answer(multi, %{id: question_id, type: "select", required: true}, attrs) do
+    changeset =
+      %SelectAnswer{}
+      |> SelectAnswer.changeset(attrs)
+      |> validate_length(:selected_options, min: 1)
+
+    multi
+    |> Multi.insert(question_id, changeset)
+  end
+
+  defp create_answer(multi, %{id: question_id, type: "select", required: false}, attrs) do
+    changeset =
+      %SelectAnswer{}
+      |> SelectAnswer.changeset(attrs)
+
+    multi
+    |> Multi.insert(question_id, changeset)
+  end
+
+  defp create_answer(multi, %{id: question_id, type: "fill", required: true}, attrs) do
+    changeset =
+      %FillAnswer{}
+      |> FillAnswer.changeset(attrs)
+      |> validate_required(:content)
+
+    multi
+    |> Multi.insert(question_id, changeset)
+  end
+
+  defp create_answer(multi, %{id: question_id, type: "fill", required: false}, attrs) do
+    changeset =
+      %FillAnswer{}
+      |> FillAnswer.changeset(attrs)
+
+    multi
+    |> Multi.insert(question_id, changeset)
+  end
+
+  defp get_question(id, survey) do
+    survey.questions
+    |> Enum.find(fn question -> question.id == id end)
   end
 
   def changeset(%Response{} = response, attrs) do
