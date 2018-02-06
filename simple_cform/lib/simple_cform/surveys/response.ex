3bd2a756579b59de2884b4460a5b8a1a06e2e68a
Yiming Chen <dsdshcym@gmail.com>
2018-02-06 20:21:24 +0800

    feature: validate all questions were answered in Response

--- simple_cform/lib/simple_cform/surveys/response.ex
+++ simple_cform/lib/simple_cform/surveys/response.ex
@@ -15,4 +15,31 @@ defmodule SimpleCform.Surveys.Response do
     |> validate_required([:survey_id, :answers])
     |> validate_all_questions_were_answered()
   end
+
+  defp validate_all_questions_were_answered(changeset) do
+    with {_, survey_id} <- changeset |> fetch_field(:survey_id),
+         survey <- survey_id |> Surveys.get_survey!() do
+      changeset
+      |> validate_change(:answers, fn :answers, answers ->
+        asked_questions_ids = survey.questions |> Enum.map(& &1.id) |> Enum.sort()
+
+        answered_questions_ids =
+          answers |> Enum.map(&(&1[:question_id] || &1["question_id"])) |> Enum.sort()
+
+        unanswered_questions_ids =
+          asked_questions_ids
+          |> Enum.filter(&(&1 not in answered_questions_ids))
+
+        if Enum.empty?(unanswered_questions_ids) do
+          []
+        else
+          [
+            answers:
+              {"some questions were not answered",
+               [unanswered_questions_ids: unanswered_questions_ids]}
+          ]
+        end
+      end)
+    end
+  end
 end
--- simple_cform/test/simple_cform/surveys/response_test.exs
+++ simple_cform/test/simple_cform/surveys/response_test.exs
@@ -14,5 +14,22 @@ defmodule SimpleCform.Surveys.ResponseTest do
 
       assert "can't be blank" in errors_on(changeset)[:answers]
     end
+
+    test "validation failed if any question in the survey was not answered" do
+      changeset = %Response{} |> Response.changeset(%{survey_id: 1, answers: []})
+
+      assert "some questions were not answered" in errors_on(changeset).answers
+    end
+
+    test "validation passed if all questions in the survey were answered" do
+      changeset =
+        %Response{}
+        |> Response.changeset(%{
+          survey_id: 1,
+          answers: [%{question_id: 1, selected_options: []}, %{question_id: 2, content: ""}]
+        })
+
+      assert changeset.valid?
+    end
   end
 end
